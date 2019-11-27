528be8b89fe891a0ab5a14bb0ae1554c850879ea
charlie <18823788683@163.com>
2019-11-27 20:04:26 +0800

    创建了consumer服务

---
+++ microservices-consumer-dept-801/src/main/java/com/charlie/Application801.java
@@ -0,0 +1,12 @@
+package com.charlie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application801 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application801.class, args);
+	}
+}
--- microservices-consumer-dept-801/src/main/java/com/charlie/controller/DeptController_Consumer.java
+++ microservices-consumer-dept-801/src/main/java/com/charlie/controller/DeptController_Consumer.java
@@ -1,32 +1,27 @@
 package com.charlie.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.charlie.api.Dept;
-import com.charlie.service.DeptService;
 
 @RestController
-@RequestMapping("/provider")
-public class DeptController {
+@RequestMapping("/consumer")
+public class DeptController_Consumer {
+	
+	private static final String URL = "http://localhost:8001/provider/";
 
 	@Autowired
-	private DeptService service;
+	private RestTemplate restTemlate;
 	
 	@RequestMapping(value = "/find/dept/{id}", method = RequestMethod.GET)
 	public Dept getDeptById(@PathVariable("id") long id) {
 		
-		return this.service.getDept(id);  
+		return this.restTemlate.getForObject(URL + "find/dept/" + id, Dept.class);
 	}
 	
-	@RequestMapping(value = "/find/dept/list", method = RequestMethod.GET)
-	public List<Dept> list(){
-		
-		return this.service.list();
-	}
 }
