118137a349f4c2dc5665d9140db09b198f526730
charlie <18823788683@163.com>
2019-12-04 19:55:52 +0800

    创建了zuul网关

---
+++ microservices-zuul-getway-9527/src/main/java/com/charlie/Application9527.java
@@ -0,0 +1,16 @@
+package com.charlie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class Application9527 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application9527.class, args);
+	}
+}
